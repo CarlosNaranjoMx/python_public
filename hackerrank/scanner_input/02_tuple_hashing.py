8e5c5b7dc5559c81415d92835152d50294fc5456
Carlos Naranjo <carlosnaranjo@ciencias.unam.mx>
2023-06-27 06:17:09 -0600

    06 Modificación
    Manipulación de las tuplas

--- hackerrank/scanner_input/02_tuple_hashing.py
+++ hackerrank/scanner_input/02_tuple_hashing.py
@@ -3,5 +3,4 @@ if __name__ == '__main__':
     integer_list = tuple(map(int, input().split()))  # Lee los enteros y crea la tupla
 
     result = hash(integer_list)  # Calcula el hash de la tupla
-
     print(result)  # Imprime el resultado del hash
