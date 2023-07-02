73865654d9f9e7c101db1665b0c9426fc1bf603b
Carlos Naranjo <carlosnaranjo@ciencias.unam.mx>
2023-07-01 18:14:44 -0600

    12 Agregacion
    agegando curso de coursera

---
+++ python_coursera/main.py
@@ -0,0 +1,13 @@
+def imc(masa, estatura):
+  imc = masa / estatura**2
+  # desde aquí hacia abajo debes modificar el programa
+  # modifica la variable imc
+  # recuerda que los datos están en las variables masa y estatura
+  return imc
+
+def velocidad(distancia, tiempo):
+  resultado = "La velocidad es {} km/h o {} m/s".format((tiempo/3600)/distancia,tiempo(distancia/1000))
+  # desde aquí hacia abajo debes modificar el programa
+  # modifica la variable resultado
+  # recuerda que los datos están en las variables distancia y tiempo
+  return resultado
